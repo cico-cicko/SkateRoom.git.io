1a12d8ff923ac73b2aa13e109cec73147977b428
cico-cicko <78498487+cico-cicko@users.noreply.github.com>
2022-11-27 01:44:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* --Тоggle Menue JavaScript-- */ 
+var navLinks = document.getElementById("navLinks");
+function showMenue(){
+    navLinks.style.right ="0";        
+}
+function hideMenue(){
+    navLinks.style.right = "-400px";
+}
